0c437a985ee071dd9be1c6b68068dc34d54468e8
Seymone Gugneja <seymonegug@gmail.com>
2023-09-12 18:02:48 -0400

    Create geometry.py

---
+++ geometry.py
@@ -0,0 +1,13 @@
+from math import pi
+
+def area_of_rectangle(length, width):
+    """
+    This function returns the area of a rectangle given the length and width
+    """
+    return length * width
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle given a radius
+    """
+    return (pi * (radius ** 2))
